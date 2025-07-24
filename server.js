5cc5a93427129d58c1b5d954a4dd5cc5d2e6c273
Eduarda Pereira <eduarda.santil@outlook.pt>
2025-07-24 09:18:21 +0100

    Update Pug view configuration and index rendering
    Changed Pug view directory to './views' and simplified the index route to render the 'index' template without passing variables. Also moved Pug require statement for consistency.

--- server.js
+++ server.js
@@ -2,25 +2,21 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
+let pug= require('pug');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.route('/').get((req, res) => {
-  res.render('index', {
-    title: 'Home Page',
-    message: 'Hello',
-    showLogin: true,
-    showRegistration: true,
-    showSocialAuth: true
-  });
+  res.render('index');
 });
 
 
