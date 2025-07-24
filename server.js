59558f817a1591de1206a4fa0b5d8a8e9edd0ff9
Eduarda Pereira <eduarda.santil@outlook.pt>
2025-07-24 09:25:28 +0100

    Update views path and minor code style in server.js
    Changed the views directory to './views/pug' for Pug templates and updated string quotes for consistency. Removed unused dotenv and myDB imports.

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 'use strict';
-require('dotenv').config();
+
 const express = require('express');
-const myDB = require('./connection');
+
 let pug= require('pug');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
-app.set('views', './views')
+app.set('views', './views/pug')
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index')
 });
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
